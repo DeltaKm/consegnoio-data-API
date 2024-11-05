26b4dfcd0c7c1655e6e900da57f359203ff1d190
Claudio Martinez <claudiomartinez@192.168.1.14>
2024-11-05 11:22:44 +0100

    update user zValidation

--- lib/zod.ts
+++ lib/zod.ts
@@ -1,17 +1,18 @@
 import { string, z } from "zod"
 
-const messageMin = "La lunghezza deve essere di almeno caratteri "
-const messageMax = "La lunghezza massima non deve superare caratteri "
+const alertMin = "La lunghezza deve essere di almeno caratteri "
+const alertMax = "La lunghezza massima non deve superare caratteri "
 
 
 export const userSchema = z.object({
-    idSlug: z.string().min(8,messageMin + "8").max(8, messageMax + "8"),
+    idSlug: z.string().min(8, alertMin + "8").max(8, alertMax + "8"),
     email: z.string()
     .min(1, { message: "Questo capodo deve essere riempito" })
     .email("Email non valida")
     .refine((e) => e === "[email protetta]", "Questa email non è nel database"),
-    usernname: z.string().min(8,messageMin + "8").max(8, messageMax + "8"),
-    password: z.string().min(8,messageMin + "8").max(8, messageMax + "8"),
+    usernname: z.string().min(5, alertMin + "5").max(10, alertMax + "10"),
+    password: z.string().min(8, alertMin + "8").max(16, alertMax + "16"),
 });
+
 
 export type UserSchema = z.infer<typeof userSchema>;
