a23d3b61bc4367dea2b398a3893a348dfc55190b
Claudio Martinez <claudiomartinez@192.168.1.14>
2024-11-05 11:15:55 +0100

    update schema usere & and first zod validation

--- lib/zod.ts
+++ lib/zod.ts
@@ -1,12 +1,17 @@
-import { z } from "zod"
+import { string, z } from "zod"
+
+const messageMin = "La lunghezza deve essere di almeno caratteri "
+const messageMax = "La lunghezza massima non deve superare caratteri "
 
 
-export const listSchema = z.object({
-    title: z.string().min(1, "Titolo richiesto").max(100, "Il titolo non deve superare i 100 caratteri"),
-
-    description: z.string().max(500, "La descrizione non deve superare i 500 caratteri").optional(),
-
-    isCompleted: z.boolean().default(false),
+export const userSchema = z.object({
+    idSlug: z.string().min(8,messageMin + "8").max(8, messageMax + "8"),
+    email: z.string()
+    .min(1, { message: "Questo capodo deve essere riempito" })
+    .email("Email non valida")
+    .refine((e) => e === "[email protetta]", "Questa email non è nel database"),
+    usernname: z.string().min(8,messageMin + "8").max(8, messageMax + "8"),
+    password: z.string().min(8,messageMin + "8").max(8, messageMax + "8"),
 });
 
-export type ListSchema = z.infer<typeof listSchema>;
+export type UserSchema = z.infer<typeof userSchema>;
