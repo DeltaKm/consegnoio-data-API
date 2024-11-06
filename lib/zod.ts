7ece42cc3b2ab18d6fb8bb9aa28f95277ba40f0b
Claudio Martinez <claudiomartinez@192.168.1.14>
2024-11-06 11:33:58 +0100

    mod type model & add base z validatation

--- lib/zod.ts
+++ lib/zod.ts
@@ -20,7 +20,30 @@ export const anagraficaUserType = z.object({
     tel: z.string().min(8, alertMin + "8").max(8, alertMax + "20"),
 });
 
+export const cordTypeSchema = z.object({
+    lat: z.number().finite(),
+    long: z.number().finite(),
+});
 
+export const datiFiscaliSchema = z.object({
+    codiceFiscale: z.string().min(16, alertMin + "16").max(16, alertMax + "16"),
+    ragioneScociale: z.string().min(8, alertMin + "1").max(8, alertMax + "30"),
+    partitaIVA: z.string().min(11, alertMin + "11").max(11, alertMax + "28"),
+    codiceSDI: z.string().min(7, alertMin + "7").max(7, alertMax + "7"),
+    pec: z.string()
+    .min(1, { message: "Questo capodo deve essere riempito" })
+    .email("Email non valida")
+    .refine((e) => e === "[email protetta]", "Questa email non è nel database"),
+});
+
+export const indirizzoType = z.object({
+    via: z.string().min(2, alertMin + "2").max(30, alertMax + "30"),
+    numeroCivico: z.string().min(1, alertMin + "1").max(5, alertMax + "5"),
+    provincia: z.string().min(2, alertMin + "2").max(12, alertMax + "12"),
+    provinciaSigla: z.string().min(2, alertMin + "2").max(16, alertMax + "2"),
+    citta: z.string().min(2, alertMin + "2").max(14, alertMax + "14"),
+    cap: z.number().min(5, alertMin + "5").max(5, alertMax + "5"),
+});
 
 
 export type AnagraficaUserType = z.infer<typeof anagraficaUserType>;
