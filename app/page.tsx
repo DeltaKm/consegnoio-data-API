0ab72aa549fa3c728fe1162740e994ebde64b979
Claudio Martinez <claudiomartinez@192.168.1.14>
2024-11-07 11:53:04 +0100

    Add shadCN Card RestAPI f(GET) => comuni

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,7 @@ import RouteToID from "@/components/route-to-id";
 
 
 export default function Home() {
-  return (
-    <div className="max-w-7x1 flex flex-col gap-10 mx-auto p-10">
-      <div className="flex justify-between items-center">
-        <FetchComuni />       
-      </div>    
-    </div>
+  return (    
+        <FetchComuni />  
   );
 }
--- components/fetch-comuni.tsx
+++ components/fetch-comuni.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Comuni } from "@prisma/client";
+import useSWR from "swr";
+
+
 import {
   Card,
   CardContent,
@@ -7,11 +11,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-
-import { Comuni } from "@prisma/client";
-import useSWR from "swr";
-import { TypeOf, z } from "zod"
-import DeleteList from "./delete-list";
 
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
@@ -27,60 +26,41 @@ export default function FetchComuni() {
 
 
 
-  // if(isLoading)
+  if(isLoading)
 
-  //   return(
-  //       <div className="flex justify-center items-center h-40 bg-white">
-  //           <div className="relative w-12 h-12">
-  //               <div className="absolute w-12 h-12 border-4 border-primary rounded-full animate-spin border-t-transparent"></div>
-  //               <div className="absolute w-12 h-12 border-4 border-primary roundend-full animate-ping opacity-25"></div>
-  //           </div>
-  //       </div>
-  //   );
+    return(
+        <div className="flex justify-center items-center h-40 bg-white">
+            <div className="relative w-12 h-12">
+                <div className="absolute w-12 h-12 border-4 border-primary rounded-full animate-spin border-t-transparent"></div>
+                <div className="absolute w-12 h-12 border-4 border-primary roundend-full animate-ping opacity-25"></div>
+            </div>
+        </div>
+    );
 
-  //   if(error) return <div>Errore di caricamento</div>;
+    if(error) return <div>Errore di caricamento</div>;
 
-    const List = comuniData || [];
-
+    const view = comuniData || []
+    
+    view.map((e) =>  e.cap )
   return (
+<>
+   
+       <h1 className="text-4x1 font-bold my-4 pl-">Test REST API GET</h1>
 
-    <div className="max-w-7x1 flex flex-col gap-10 mx-auto p-10">
-    <div className="flex justify-between items-center">
-      <h1 className="text-4x1 font-bold">Test REST API GET</h1>
-       
-    </div>     
-    
-  </div>
-    
-    // <h1>{List.map((comuni) =>(
-    //   <h1> {comuni.denominazioneIta}</h1>
-    // ))}</h1>
-    // <div className="space-y-4">
-    //   {List.length === 0 ? (
-    //     <Card>
-    //       <CardContent className="text-center py-10">
-    //         <p className="text-muted-foreground">Aggiungi comuni</p>
-    //       </CardContent>
-    //     </Card>
-    //   ) : (
-    //     List.map((comuni) => (
-    //       <Card className="group relative" key={comuni.id}>
-    //         <div className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity">
-    //           {/* <DeleteList id={comuni.id} /> */}
-    //         </div>
-    //         <CardHeader>
-    //           <CardTitle>
-    //             <span >
-    //               {comuni.denominazioneIta}
-    //             </span>
-    //           </CardTitle>
-    //         </CardHeader>
-    //         <CardContent>
-    //           <p>{comuni.cap}</p>
-    //         </CardContent>
-    //       </Card>
-    //     ))
-    //   )}
-    // </div>
+   { view.map((e) =>  (
+            <Card className="group relative" key={e.id}>
+                <CardHeader>
+                <CardTitle>
+                    {e.denominazioneIta}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{e.siglaProvincia}</p>
+              </CardContent>
+              </Card>
+            ) )}
+  </>
+  
+
   );
 }
