df3a55ecfa7ba6107242862c3afeac46a52c0540
Claudio Martinez <clamart85hs@gmail.com>
2025-01-18 22:37:53 +0100

    fix backend 6

--- lib/zod.ts
+++ lib/zod.ts
@@ -82,19 +82,19 @@ export const userSchema = z.object({
 
 
 
-export const testDeliverySchema = z.object({  
-  name: z.string().min(1, "Il nome è obbligatorio"),
-  pickupAddress: z.string().min(1, "L'indirizzo di ritiro è obbligatorio"),
-  deliveryAddress: z.string().min(1, "L'indirizzo di consegna è obbligatorio"),
-  totalDistance: z.string().min(1, "La distanza totale è obbligatoria"),
-  deliveryType: z.string().min(1, "Il tipo di consegna è obbligatorio"),
-  peso: z.number().min(0, "Il peso deve essere maggiore o uguale a 0"),
-  numeroColli: z.number().min(1, "Il numero di colli deve essere almeno 1"),
-  compensation: z.number().positive("La compensazione deve essere positiva"),
-  isAssigned: z.boolean(), // Campo obbligatorio
-  isCompleted: z.boolean(), // Campo obbligatorio
-});
-
+export const testDeliverySchema = z.object({
+    name: z.string().min(1),
+    pickupAddress: z.string().min(1),
+    deliveryAddress: z.string().min(1),
+    totalDistance: z.string().min(1),
+    deliveryType: z.string().min(1),
+    peso: z.number(),
+    numeroColli: z.number(),
+    compensation: z.number(),
+    isAssigned: z.boolean(),
+    isCompleted: z.boolean(),
+  }).partial(); // Rende tutti i campi opzionali
+  
   
 export const orderSchema = z.object({
     hashQrFirma: z.string().min(8, alertMin + "8").max(8, alertMax + "8"),
