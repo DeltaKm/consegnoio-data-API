1476af9de50b1bc6b72145d698024226de315f22
Claudio Martinez <clamart85hs@gmail.com>
2025-02-08 17:11:44 +0100

    remove old user asset for build alpha

--- lib/zod.ts
+++ lib/zod.ts
@@ -1,6 +1,5 @@
 
-import { preset } from "swr/_internal";
-import { object, z } from "zod"
+import { z } from "zod"
 
 
 const alertMin = "La lunghezza deve essere di almeno caratteri "
@@ -21,35 +20,10 @@ export const testDeliverySchema = z.object({
     isCompleted: z.boolean(),
   }).partial(); 
 
-export const raiderPersonalDataTypeSchema = z.object({
-  name: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  surname: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  email: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  birthDay: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  businessId: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  isActive: z.boolean(),
-}).partial();
-
-export const zonePrefernceTypeSchema = z.object({
-  radiusKm: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  distanceKm: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-}).partial()
-
-export const qrValidation = z.object({
-  validation: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  config: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  presetMultiDelivery: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-  postiionVal: z.string().min(1, alertMin + "1").max(30, alertMax + "30"),
-}).partial();
 
 export type TestDeliverySchema = z.infer <typeof testDeliverySchema>;
-export type RaiderPersonalDataTypeSchema = z.infer <typeof raiderPersonalDataTypeSchema>;
-export type ZonePrefernceTypeSchema = z.infer <typeof zonePrefernceTypeSchema>;
-export type QrValidation = z.infer <typeof qrValidation>;
 
 export const schemas = {    
     testDeliverySchema,
-    raiderPersonalDataTypeSchema,
-    zonePrefernceTypeSchema,
-    qrValidation,
+
 };
