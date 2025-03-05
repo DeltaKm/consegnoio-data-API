1dd17d3baadb8b18e31ffeeeefd1a7436932a1fe
Claudio Martinez <clamart85hs@gmail.com>
2025-03-05 16:29:57 +0100

    restyle route utils/getstatusvalue

--- app/api/v1/utils/getstatusvalue/route.ts
+++ app/api/v1/utils/getstatusvalue/route.ts
@@ -1,26 +1,29 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  try {
-    const statusValues = await prisma.statusValue.findMany({
-      select: {
-        label: true,
-        value: true,
-      },
-    });
-
-    const responseData: Record<string, string> = statusValues.reduce((acc, curr) => {
-      acc[curr.label] = curr.value;
-      return acc;
-    }, {} as Record<string, string>);
-
-    return NextResponse.json(responseData);
-  } catch (error) {
-    console.error("Errore nel recuperare gli status value", error);
-    return NextResponse.json(
-      { message: "Errore nel recuperare gli status value" },
-      { status: 500 }
-    );
+enum StatusCodes {
+    Success = 200,
+    NotFound = 404,
+    InternalServerError = 500,
   }
-}
+
+
+export async function GET() {
+    try {
+      const statusValue = await prisma.statusValue.findMany({
+        orderBy: { label: "asc" },
+        select: {
+            label: true,
+            value: true,
+          },
+      });
+      return NextResponse.json(statusValue, { status: StatusCodes.Success });
+  
+    } catch (error) {
+      console.error("Errore durante il fetch delle consegne", error);
+      return NextResponse.json(
+        { message: "Errore durante il fetch delle consegne" },
+        { status: StatusCodes.InternalServerError }
+      );
+    }
+  }
